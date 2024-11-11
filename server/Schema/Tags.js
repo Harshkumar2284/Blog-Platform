562cd09ae74db86e4699a415f58da570c2586f0e
Harshkumar2284 <140580320+Harshkumar2284@users.noreply.github.com>
2024-11-11 21:08:12 +0530

    Done

--- server/Schema/Tags.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-
-const tags = mongoose.Schema({
-    tag: {type:String, required:true},
-    count: {type:Number, require:true}
-})
-
-module.exports = mongoose.model("Tags", tags)
